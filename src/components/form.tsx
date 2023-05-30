12587c092b7697c663ac868196b7ef2af5562da9
Stablest <marlonlocci@hotmail.com>
2023-05-30 16:00:20 -0300

    Changed HTML Elements to % size

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react"
+import { useState } from "react"
 
 type FormProps = {
     linkError: string,
@@ -18,12 +18,12 @@ export function Form({ children, shorten, linkError }: FormProps) {
     return (
         <>
             <form action="" className="flex flex-col w-full h-full justify-center">
-                <div className="flex gap-x-8 mx-auto">
-                    <div className="flex flex-col relative">
-                        <input type="url" name="url input" placeholder='Shorten a link here...' className={`w-[48rem] h-16 bg-white rounded-xl px-8 text-xl ${border}`} value={inputValue} onChange={handleChange} />
+                <div className="flex gap-x-8 mx-auto w-full px-16">
+                    <div className="flex flex-col relative w-full">
+                        <input type="url" name="url input" placeholder='Shorten a link here...' className={` h-16 bg-white rounded-xl px-8 text-xl ${border}`} value={inputValue} onChange={handleChange} />
                         <span className="text-red text-md italic text-sm absolute top-[4.25rem]">{children}</span>
                     </div>
-                    <button className="bg-cyan rounded-xl w-48 h-16 text-white text-xl" onClick={(e) => shorten(e, inputValue)}>Shorten it!</button>
+                    <button className="bg-cyan rounded-xl text-white text-xl w-3/12" onClick={(e) => shorten(e, inputValue)}>Shorten it!</button>
                 </div>
             </form>
         </>
