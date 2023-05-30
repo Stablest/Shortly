d6207d281c16fa80d7dbad12e4ab73ea33804710
Stablest <marlonlocci@hotmail.com>
2023-05-30 01:49:42 -0300

    added Shortener component, removed application logic from page route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Box } from "../components/box"
+import { Shortener } from "../components/shortener"
 
 export default function Home() {
   return (
@@ -12,25 +13,17 @@ export default function Home() {
             <span className=" text-[5rem] font-bold leading-[1.1]">More than just shorter links</span>
             <span className="text-grayish-violet text-xl w-[80%] leading-[1.8]">Build your brand's recognition and get detailed insights on how your links are
               performing.</span>
-            <button className="w-48 h-14 bg-cyan rounded-full text-white text-lg font-bold">Get Started</button>
+            {/* <button className="w-48 h-14 bg-cyan rounded-full text-white text-lg font-bold">Get Started</button> */}
           </div>
           <img src="./images/illustration-working.svg" alt="Person using a computer" className="h-[30rem]" />
         </section>
-
         <section className="bg-grayish-violet bg-opacity-[0.12] w-full px-40 pb-44 flex flex-col items-center">
-          <div className="bg-[url('/images/bg-shorten-desktop.svg')] bg-cover bg-no-repeat bg-dark-violet w-full h-44 mt-[-5rem] flex justify-center items-center rounded-xl">
-            <form action="" method="get" className="w-full h-full flex justify-center items-center gap-x-8">
-              <input type="url" name="" id="" placeholder='Shorten a link here...' className="w-[70%] h-[40%] bg-white rounded-xl px-8 text-xl" />
-              <button className="bg-cyan rounded-md w-[16%] h-[40%] text-white text-xl">Shorten it!</button>
-            </form>
-          </div>
-
+          <Shortener></Shortener>
           <div className="flex flex-col my-32 w-[32rem] text-center">
             <span className="text-4xl font-bold mx-auto">Advanced Statistics</span>
             <span className="text-grayish-violet my-6">Track how your links are performing across the web with our
               advanced statistics dashboard.</span>
           </div>
-
           <div className="flex gap-x-8">
             <Box url="./images/icon-brand-recognition.svg" alt="graph" title='Brand Recognition'>
               Boost your brand
@@ -51,10 +44,8 @@ export default function Home() {
         </section>
         <section className="bg-[url('/images/bg-boost-desktop.svg')] h-60 w-full flex flex-col bg-dark-violet justify-center items-center gap-y-6">
           <span className="text-4xl font-black text-white">Boost your links today</span>
-          <button className="w-48 h-14 bg-cyan rounded-full text-white text-lg font-bold">Get Started</button>
-
+          {/* <button className="w-48 h-14 bg-cyan rounded-full text-white text-lg font-bold">Get Started</button> */}
         </section>
-
       </main>
       <footer className="w-full bg-very-dark-violet text-white">
         <div className="px-40 py-20">
@@ -62,7 +53,7 @@ export default function Home() {
           <div className="w-full flex text-sm mt-4 gap-x-1">
             Challenge by
             <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-            >Frontend Mentor.</a> Coded by <a href="#"> Marlon Locci</a>.
+            >Frontend Mentor.</a> Coded by <a href="https://github.com/Stablest" className="text-cyan" target="_blank"> Marlon Locci.</a>
           </div>
         </div>
       </footer>
